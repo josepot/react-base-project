259ebd11b68c2079d276ac5c1c956df7a19f140b
Josep M Sobrepere <josepot@gmail.com>
2018-09-22 23:58:43 +0200

    remove unnecessaries s

--- src/App/List/ListLoader.js
+++ src/App/List/ListLoader.js
@@ -1,13 +1,12 @@
 import {complement} from 'ramda';
 import {compose} from 'recompose';
 import {connect} from 'react-redux-lean';
-import {createStructuredSelector} from 'reselect';
 import {isListLoadingSelector as isLoading} from 'modules/items';
 import {Loader} from 'components';
 
 export default compose(
   connect(
-    createStructuredSelector({hidden: complement(isLoading)}),
+    {hidden: complement(isLoading)},
     {}
   )
 )(Loader);
