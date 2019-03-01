19b333ede9e095100b4a9deef6f84770ef97dbc9
Josep M Sobrepere <josepot@gmail.com>
2019-03-02 00:55:52 +0100

    Improve withRedux

--- src/lib/hocs/withRedux.js
+++ src/lib/hocs/withRedux.js
@@ -2,79 +2,40 @@ import React, {useContext, useMemo} from 'react';
 import {context} from 'ReduxProvider';
 
 const emptyObj = {};
-const mapObj = (obj, mapper) =>
-  Object.keys(obj).reduce((res, key) => {
-    res[key] = mapper(obj[key], key, obj);
-    return res;
-  }, {});
+const alwaysEmpty = () => emptyObj;
+const defaultMapper = (stateProps, actionProps, externalProps) => ({
+  ...externalProps,
+  ...stateProps,
+  ...actionProps,
+});
 
-const shallowCompare = (a, b) => {
-  if (a === b) return true;
-  const aKeys = Object.keys(a);
-  const bKeys = Object.keys(b);
-  if (aKeys.length !== bKeys.length) return false;
+export default (
+  fromStateProps_,
+  fromActionProps = emptyObj,
+  mapper = defaultMapper
+) => {
+  const dependsOnProps = fromStateProps_ && fromStateProps_.length !== 1;
+  const fromStateProps = !fromStateProps_ ? alwaysEmpty : fromStateProps_;
 
-  for (let i = 0; i < aKeys.length; i += 1) {
-    if (a[aKeys[i]] !== b[aKeys[i]] || a[bKeys[i]] !== b[bKeys[i]]) {
-      return false;
-    }
-  }
-  return true;
-};
+  return BaseComponent => props => {
+    const dependantProps = dependsOnProps ? props : emptyObj;
 
-const memoizeObjectFn = fn => {
-  let prevResult;
-  return (...args) => {
-    const res = fn(...args);
-    if (prevResult && shallowCompare(prevResult, res)) return prevResult;
-    prevResult = res;
-    return res;
-  };
-};
+    const {state, dispatch} = useContext(context);
+    const stateProps = useMemo(() => fromStateProps(state, dependantProps), [
+      state,
+      dependantProps,
+    ]);
+    const actionProps = useMemo(() => {
+      const res = {};
+      Object.entries(fromActionProps).forEach(([name, aCreator]) => {
+        res[name] = (...args) => dispatch(aCreator(...args));
+      });
+      return res;
+    }, [dispatch]);
 
-export default (fromStateProps_, fromActionProps, mapper) => {
-  const isObject = typeof fromStateProps_ === 'object';
-  const dependsOnProps =
-    fromStateProps_ &&
-    (isObject ? Object.values(fromStateProps_) : [fromStateProps_]).some(
-      fn => fn.length !== 1
+    return useMemo(
+      () => <BaseComponent {...mapper(stateProps, actionProps, props)} />,
+      [actionProps, props, stateProps]
     );
-
-  const fromStateProps = !fromStateProps_
-    ? () => emptyObj
-    : isObject
-    ? memoizeObjectFn((...args) => mapObj(fromStateProps_, fn => fn(...args)))
-    : fromStateProps_;
-
-  return BaseComponent => {
-    const baseFn = BaseComponent.prototype
-      ? p => <BaseComponent {...p} /> // eslint-disable-line
-      : BaseComponent;
-    let actionProps = fromActionProps ? null : emptyObj;
-
-    return props => {
-      const {state, dispatch} = useContext(context);
-      const dependantProps = dependsOnProps ? props : emptyObj;
-      const stateProps = useMemo(() => fromStateProps(state, dependantProps), [
-        state,
-        dependantProps,
-      ]);
-
-      if (!actionProps) {
-        actionProps = mapObj(fromActionProps, fn => (...args) =>
-          dispatch(fn(...args))
-        );
-      }
-
-      return useMemo(
-        () =>
-          baseFn(
-            mapper
-              ? mapper(stateProps, actionProps, props)
-              : {...props, ...stateProps, ...actionProps}
-          ),
-        [stateProps] // eslint-disable-line
-      );
-    };
   };
 };
