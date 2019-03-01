14e6bc72d7124577b09287521fd794ad64764711
Josep M Sobrepere <josepot@gmail.com>
2019-03-02 00:56:15 +0100

    Improve List

--- src/App/List/Item/Item.Container.js
+++ src/App/List/Item/Item.Container.js
@@ -1,11 +1,9 @@
-import React, {useMemo, memo} from 'react';
+import React, {useMemo} from 'react';
 import {itemSelector} from 'modules/items';
 import {useReduxState} from 'lib/hooks';
 import Item from './Item.Component';
 
-const MItem = memo(Item);
-
 export default props => {
-  const finalProps = useReduxState(itemSelector, props);
-  return useMemo(() => <MItem {...finalProps} />, [finalProps]);
+  const stateProps = useReduxState(itemSelector, props);
+  return useMemo(() => <Item {...stateProps} />, [stateProps]);
 };
--- src/App/List/List.Container.js
+++ src/App/List/List.Container.js
@@ -1,13 +1,28 @@
-import {withRedux} from 'hocs';
-import {useOnScrollBottom} from 'hooks';
+import React, {useMemo} from 'react';
+import {useReduxState, useReduxActions, useOnScrollBottom} from 'hooks';
+import createSelector from 'redux-views';
 import {idsListSelector, requestItems} from 'modules/items';
 import ListComponent from './List.Component';
 
-export default withRedux(
-  {itemIds: idsListSelector},
-  {requestItems},
-  (state, actions) => ({
-    ...state,
-    onScroll: useOnScrollBottom(actions.requestItems),
-  })
-)(ListComponent);
+const selector = createSelector(
+  [idsListSelector],
+  itemIds => ({itemIds})
+);
+
+const actions = {requestItems};
+
+export default () => {
+  const stateProps = useReduxState(selector);
+  const actionProps = useReduxActions(actions);
+  return useMemo(
+    () => (
+      <ListComponent
+        {...{
+          ...stateProps,
+          onScroll: useOnScrollBottom(actionProps.requestItems),
+        }}
+      />
+    ),
+    [actionProps.requestItems, stateProps]
+  );
+};
