55132c7cba25252f0fb0d1a94cb54173253ae6a1
Josep M Sobrepere <josepot@gmail.com>
2019-03-02 00:55:07 +0100

    Improve withForm

--- src/lib/hocs/withForm.js
+++ src/lib/hocs/withForm.js
@@ -1,11 +1,12 @@
 import {always, assoc, assocPath, map} from 'ramda';
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 const resetState = (props, mapPropsToValues) => ({
   values: mapPropsToValues(props),
   errors: {},
   touched: {},
   isSubmitting: false,
+  isSubmitPending: false,
 });
 
 export default ({
@@ -13,49 +14,71 @@ export default ({
   validate,
   handleSubmit,
 }) => BaseComponent => props => {
-  const [state, setState] = useState(resetState(props, mapPropsToValues));
+  const [state, setState] = useState(() => resetState(props, mapPropsToValues));
 
-  const onBlur = e => {
-    const {name} = e.target;
-    const {touched} = state;
-    if (touched[name]) return;
-    setState(assocPath(['touched', name], true));
-  };
+  const onBlur = useCallback(
+    e => {
+      const {name} = e.target;
+      setState(prevState =>
+        prevState.touched[name]
+          ? prevState
+          : assocPath(['touched', name], true, prevState)
+      );
+    },
+    [setState]
+  );
 
-  const onChange = e => {
-    const {name, value} = e.target;
-    setState(({values, ...rest}) => {
-      const nextValues = assoc(name, value, values);
-      const nextErrors = validate(nextValues);
-      return {
-        ...rest,
-        values: nextValues,
-        errors: nextErrors,
-      };
-    });
-  };
+  const onChange = useCallback(
+    e => {
+      const {name, value} = e.target;
+      setState(({values, ...rest}) => {
+        const nextValues = assoc(name, value, values);
+        const nextErrors = validate(nextValues);
+        return {
+          ...rest,
+          values: nextValues,
+          errors: nextErrors,
+        };
+      });
+    },
+    [setState]
+  );
 
-  const reset = () => {
-    setState(resetState(props, mapPropsToValues));
-  };
+  const onFormSubmit = useCallback(
+    e => {
+      e.preventDefault();
 
-  const onSubmit = e => {
-    e.preventDefault();
+      setState(({isSubmitting, values}) => {
+        const errors = validate(values);
+        const touched = map(always(true), values);
+        const isSubmitPending = Object.keys(errors).length === 0;
+        return {values, errors, touched, isSubmitPending, isSubmitting};
+      });
+    },
+    [setState]
+  );
 
-    const {values} = state;
-    const errors = validate(values);
-    const touched = map(always(true), values);
-    const hasErrors = Object.keys(errors).length > 0;
+  const onSubmit = useCallback(
+    values =>
+      handleSubmit(values, {
+        props,
+        resetForm: () => setState(resetState(props, mapPropsToValues)),
+      }),
+    [setState, props]
+  );
 
-    setState(prev => ({...prev, errors, touched, isSubmitting: !hasErrors}));
-    if (hasErrors) return;
-    handleSubmit(values, {
-      props,
-      resetForm: reset,
-    });
-  };
+  useEffect(() => {
+    if (!state.isSubmitPending) return;
+    onSubmit(state.values);
+    setState(prevState => ({
+      ...prevState,
+      isSubmitting: true,
+      isSubmitPending: false,
+    }));
+  }, [state.isSubmitPending, state.values, onSubmit]);
+
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onFormSubmit}>
       <BaseComponent {...state} onChange={onChange} onBlur={onBlur} />
     </form>
   );
