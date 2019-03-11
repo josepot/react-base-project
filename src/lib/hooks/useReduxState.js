222abe27a29961defd20ac2f6b1740e3620f375a
Josep M Sobrepere <josepot@gmail.com>
2019-03-12 00:59:04 +0100

    Upgrade redux-views to v0.3.0

--- src/lib/hooks/useReduxState.js
+++ src/lib/hooks/useReduxState.js
@@ -1,16 +1,14 @@
-import {useContext, useEffect, useMemo, useRef} from 'react';
+import {useContext, useEffect, useMemo} from 'react';
 import {context} from 'ReduxProvider';
 
 const emptyObj = {};
-const noUsage = [Function.prototype, Function.prototype];
 
-export default (selector, props = emptyObj) => {
-  const ref = useRef(selector.use ? selector.use() : noUsage, [selector]);
-  useEffect(() => ref.current[1], [selector]);
+export default (selector_, props = emptyObj) => {
+  const [selector, unsubscribe] = useMemo(
+    () => (selector_.use ? selector_.use() : [selector_, Function.prototype]),
+    [selector_]
+  );
+  useEffect(() => unsubscribe, [unsubscribe]);
   const {state} = useContext(context);
-  return useMemo(() => {
-    const result = selector(state, props);
-    ref.current[0]();
-    return result;
-  }, [selector, state, props]);
+  return useMemo(() => selector(state, props), [selector, state, props]);
 };
